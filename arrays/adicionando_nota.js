1128fcc51fc18e504e68347187eaaaf534901ba3
bighead991 <jadsonpimentel10@gmail.com>
2023-04-13 23:36:28 -0300

    adicionando nota

---
+++ arrays/adicionando_nota.js
@@ -0,0 +1,5 @@
+const notas = [10, 8.75, 9, 7, 9.65]
+
+notas.push(9.5)
+
+console.log(notas)
--- arrays/media.js
+++ arrays/media.js
@@ -1,8 +1,10 @@
-const nota1 = 10;
-const nota2 = 8.75;
-const nota3 = 9;
-const nota4 = 7
+// const nota1 = 10;
+// const nota2 = 8.75;
+// const nota3 = 9;
+// const nota4 = 7
 
-media = (nota1 + nota2 + nota3 + nota4) / 4;
+const notas = [10, 8.75, 9, 7, 9.65]
+
+media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
 
 console.log(media)
