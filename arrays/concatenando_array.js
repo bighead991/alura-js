3c61082b4a208041bd9202092521ae025f0270f6
Jadson Pimentel <jadsonpimentel10@gmail.com>
2023-04-16 15:24:20 -0300

    Utilizando Concat

---
+++ arrays/concatenando_array.js
@@ -0,0 +1,8 @@
+const salaJs = ["Evaldo", "Camis", "Mari"];
+
+const salaPython = ["Jadson", "Danielle"];
+
+// CONCATENA DUAS OU MAIS ARRAYS
+const salasUnificadas = salaJs.concat(salaPython);
+
+console.log(salasUnificadas)
