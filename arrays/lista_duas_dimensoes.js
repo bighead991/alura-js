571beefc2e17d10e9f275b7f83b5a43d6cbaf115
Jadson Pimentel <jadsonpimentel10@gmail.com>
2023-04-16 15:54:34 -0300

    lista-duas-dimensões

--- arrays/lista_duas_dimensoes.js
+++ arrays/lista_duas_dimensoes.js
@@ -4,4 +4,6 @@ const medias = [10, 8, 7.5, 9];
 // UNIFICANDO UMA OU MAIS LISTA EM UM NOVO ARRAY
 const listaMediaAlunos = [alunos, medias]
 
+console.log(listaMediaAlunos)
+
 console.log(`A nota do Aluno ${listaMediaAlunos[0][0]} nesse semetre foi igual à: ${listaMediaAlunos[1][0]}`)
