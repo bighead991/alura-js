9133d0c2f1c18cea712d352d0f5d11dd24dc007c
Jadson Pimentel <jadsonpimentel10@gmail.com>
2023-04-16 15:56:03 -0300

    procurando aluno

--- arrays/procurando-aluno.js
+++ arrays/procurando-aluno.js
@@ -3,3 +3,12 @@ const medias = [10, 8, 7.5, 9];
 
 const listaDeAlunosEMedias = [alunos, medias];
 
+function exibeNomeENota (aluno) {
+    if (listaDeAlunosEMedias[0].includes(aluno)) {
+        console.log(`${aluno} está cadastrado`)
+    } else {
+        console.log("Aluno não encontrado!")
+    }
+}
+
+exibeNomeENota("jadson")
