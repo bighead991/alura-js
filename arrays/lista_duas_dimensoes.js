e697d55939635a2018fd7234e477f639acf1a6d8
Jadson Pimentel <jadsonpimentel10@gmail.com>
2023-04-16 15:33:29 -0300

    Concatenando Arrays

---
+++ arrays/lista_duas_dimensoes.js
@@ -0,0 +1,7 @@
+const alunos = ["João", "Juliana", "Ana", "Caio"];
+const medias = [10, 8, 7.5, 9];
+
+// UNIFICANDO UMA OU MAIS LISTA EM UM NOVO ARRAY
+const listaMediaAlunos = [alunos, medias]
+
+console.log(`A nota do Aluno ${listaMediaAlunos[0][0]} nesse semetre foi igual à: ${listaMediaAlunos[1][0]}`)
