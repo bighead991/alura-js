3a9f3a1fdac4b9ff2e084b4e4d7d602ef788aac7
Jadson Pimentel <jadsonpimentel10@gmail.com>
2023-04-16 15:17:10 -0300

    Utilizando Splice

---
+++ arrays/atualizando_lista.js
@@ -0,0 +1,14 @@
+const nomes = ['João', 'Ana', 'Caio', 'Lara', 'Marjore', 'Leo']
+
+console.log(nomes)
+
+// RETIRA NOMES DE UMA LISTA
+nomes.splice(1, 2)
+console.log(nomes
+    )
+// RETIRA NOMES DE UMA LISTA E ADICIONAR UM NOVO VALOR 
+nomes.splice(1, 2, "Rodrigo")
+nomes.push('Jadson')
+
+
+console.log(nomes)
